13c1fad7b2d550d9b7323faa1509a43a3a0a17a5
akiyamart <akiyamah@yandex.ru>
2024-10-07 14:12:12 +0300

    add notpixel to docker

--- add_sessions_to_db.py
+++ add_sessions_to_db.py
@@ -9,24 +9,19 @@ from bot.core.tools.decorators import db_session_decorator
 @db_session_decorator
 async def add_sessions(session_names, db: AsyncSession):
     for name in session_names:
-        # Проверяем, существует ли сессия с данным именем
         stmt = select(Session).where(Session.session_name == name)
         result = await db.execute(stmt)
         existing_session = result.scalars().first()
 
-        # Если сессия не найдена, добавляем новую
-        if existing_session is None:  # Явная проверка на None
+        if existing_session is None:
             new_session = Session(session_name=name)
             db.add(new_session)
             print(f"Добавлена сессия: {name}")
 
-    # Сохраняем изменения в базе данных
     await db.commit()
     print("Все сессии добавлены и изменения сохранены в базе данных.")
 
-# Пример использования
 if __name__ == "__main__":
-    # Создание сессии для работы с БД
     session_names = os.listdir('./sessions')
     asyncio.run(
         add_sessions(session_names)
